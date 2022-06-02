935e7f8669644001129a32bdeaa53cbf8d0770e6
VaishaliAppachu <vaishaliappachu@gmail.com>
2022-06-02 12:50:47 +0530

    simple proj

---
+++ src/test/java/com/testing/Simpleproj/Demo1.java
@@ -0,0 +1,13 @@
+package com.testing.Simpleproj;
+
+import org.testng.annotations.Test;
+
+public class Demo1 
+{
+@Test
+public void demo1()
+{
+    String b = System.getProperty("hi");
+	System.out.println(b);
+}
+}
